58bc52315a0c47de139360b903b17f30ee1ddf9e
Yousef Obeida <yousef_4764@limu.edu.ly>
2026-06-23 22:22:00 +0200

    feat (tf) : added providers & variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "student_name" {
+  type        = string
+  description = "Your name for naming resources and tags"
+  default     = "yousef-Omar-Mohammed" 
+}
+
+variable "location" {
+  type    = string
+  default = "switzerlandnorth"
+}
