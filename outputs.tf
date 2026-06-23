7e5af207ddefc397c82ae46aa836cfedc8acec6f
Mohamed Ali <mohamed_3954@limu.edu.ly>
2026-06-23 22:38:59 +0200

    add outputs file

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
